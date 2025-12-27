2d243f3a05b7842c826deac88045113475ad98e1
Ghost <74954037+logicnotfound237@users.noreply.github.com>
2025-12-27 17:21:51 +0530

    made json file hiddne for Linux.

--- build/CMakeFiles/dodone.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dodone.
-# This may be replaced when dependencies are built.
--- build/external/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-external/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/ghost/AFOLDER/projects/dodone/external/ftxui/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/ghost/AFOLDER/projects/dodone/external/ftxui/include/ftxui/dom/flexbox_config.hpp
